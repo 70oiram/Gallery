a42153da5cec5853d4c98b7bafba36bcf1ab6f57
70oiram <marioliu1231@gmail.com>
2025-12-05 11:55:14 +0800

    2025.12.5 日結版
    11:55 a.m.

--- Assets/Scripts/ButtonActions.cs
+++ Assets/Scripts/ButtonActions.cs
@@ -3,7 +3,36 @@ using UnityEngine;
 public class ButtonActions : MonoBehaviour
 {
     string thisUrl = string.Empty;
-
+    public void button20()
+    {
+        thisUrl = "https://www.youtube.com/watch?v=2hYOgV3jnGw&list=PLhYG3NI3HHyseNfwIOsV2lwPLk7uO65hT&index=16";
+        Application.OpenURL(thisUrl);
+    }
+    public void button21()
+    {
+        thisUrl = "https://www.youtube.com/watch?v=zfFrAWhlUAI&list=PLhYG3NI3HHyseNfwIOsV2lwPLk7uO65hT&index=15";
+        Application.OpenURL(thisUrl);
+    }
+    public void button22()
+    {
+        thisUrl = "https://www.youtube.com/watch?v=6LOJv9vH2Wo&list=PLhYG3NI3HHyseNfwIOsV2lwPLk7uO65hT&index=14";
+        Application.OpenURL(thisUrl);
+    }
+    public void button23()
+    {
+        thisUrl = "https://www.youtube.com/watch?v=B8eE8LeIVcw&list=PLhYG3NI3HHyseNfwIOsV2lwPLk7uO65hT&index=13";
+        Application.OpenURL(thisUrl);
+    }
+    public void button24()
+    {
+        thisUrl = "https://www.youtube.com/watch?v=ANUBMh8l5G8&list=PLhYG3NI3HHyseNfwIOsV2lwPLk7uO65hT&index=12";
+        Application.OpenURL(thisUrl);
+    }
+    public void button25()
+    {
+        thisUrl = "https://www.youtube.com/watch?v=_CZeNAiUELc&list=PLhYG3NI3HHyseNfwIOsV2lwPLk7uO65hT&index=11";
+        Application.OpenURL(thisUrl);
+    }
     public void button26()
     {
         thisUrl = "https://www.youtube.com/watch?v=blIBz0f00HE&list=PLhYG3NI3HHyseNfwIOsV2lwPLk7uO65hT&index=10";
